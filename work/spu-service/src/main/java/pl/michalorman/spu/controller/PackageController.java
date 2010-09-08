4943109bd94669183a00d70759f9e580d81cbc91
Michal Orman <michal.orman@gmail.com>
2010-09-08 10:30:54 +0200

    Added REST service to SPU application. Generated ParcelScout Spring version project.

--- work/spu-service/src/main/java/pl/michalorman/spu/controller/PackageController.java
+++ work/spu-service/src/main/java/pl/michalorman/spu/controller/PackageController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ import pl.michalorman.spu.api.TrackResponse;
 import pl.michalorman.spu.service.PackageTrackService;
 
 @Controller
+@RequestMapping("/track")
 public class PackageController {
 
     private Logger logger = LoggerFactory.getLogger(PackageController.class);
@@ -20,11 +22,16 @@ public class PackageController {
     @Autowired
     private PackageTrackService parcelService;
 
-    @RequestMapping(value = "/track", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public void getPackageTrackInfo(@RequestParam Integer packageId, Model model) {
         logger.info("Get track info for package: '{}'", packageId);
         TrackResponse response = parcelService.getTrackInfo(packageId);
         model.addAttribute("response", response);
+    }
+
+    @RequestMapping(value = "/{packageId}", method = RequestMethod.GET)
+    public void getPackageTrackInfo(@PathVariable String packageId, Model model) {
+        getPackageTrackInfo(Integer.valueOf(packageId), model);
     }
 
 }
